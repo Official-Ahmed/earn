be38f1f621634ab2a8327499aab593682cdd7dbb
Official-Ahmed <39685818+Official-Ahmed@users.noreply.github.com>
2020-10-30 09:23:44 +0400

    Update func.js

--- func.js
+++ func.js
@@ -1,10 +1,23 @@
-function calc(){
-    x = document.getElementById("X").value;
-    op = document.getElementById("operation").value;
-    y = document.getElementById("Y").value;
-
-    if (op == "+")
-        document.getElementById("Result").innerHTML = "Result = "+ String( parseFloat(x)+parseFloat(y));
-    else if (op == "-")
-        document.getElementById("Result").innerHTML = "Result = "+ String( parseFloat(x)-parseFloat(y));
+function jailbreaking(){
+    if(document.cookie.indexOf('kex=0') >= 0)
+        document.location.href = '.';
+    else
+        document.cookie = 'kex=0';
+    </script>
+    <script>function print(){}</script>
+    <script src="common/exploit.js"></script>
+    <script src="common/helpers.js"></script>
+    <script src="common/malloc.js"></script>
+    <script src="common/rop.js"></script>
+    <script src="common/syscalls.js"></script>
+    <script src="common/syscalls2.js"></script>
+    <script src="jb/c-code.js"></script>
+    <script>
+    if(main_ret == 179 /* already hacked */ || main_ret == 0 /* success */)
+    {
+        alert("You're all set!");
+        read_ptr_at(0);
+    }
+    else
+        alert("Jailbreak failed! Reboot your PS4 and try again.");
 }
